bd3cb052818f887611b3d9d344c20970cbbfaffb
Fernando Concatto <fernandoconcatto@gmail.com>
2017-09-20 10:26:41 -0300

    Foco adicionado ao Numeric Input

--- PathEvolution/numericinput.cpp
+++ PathEvolution/numericinput.cpp
@@ -11,13 +11,17 @@ NumericInput::NumericInput(float width)
     text.setString(textualValue);
     text.setFont(*Util::getFont());
     text.setFillColor(sf::Color::Black);
-    text.setCharacterSize(15);
-//    text.setOrigin(0, 0);
+    text.setCharacterSize(40);
+    text.setOrigin(0, 0);
+
+    bounds.setFillColor(sf::Color::Transparent);
+    bounds.setOutlineColor(sf::Color::Magenta);
+    bounds.setOutlineThickness(2);
 }
 
 void NumericInput::processEvent(const sf::Event& event)
 {
-    if (event.type == sf::Event::TextEntered) {
+    if (event.type == sf::Event::TextEntered && focused) {
         if (event.text.unicode == '\b') {
             if (!textualValue.empty()) {
                 if (textualValue.back() == '.') {
@@ -31,11 +35,11 @@ void NumericInput::processEvent(const sf::Event& event)
             }
         } else {
             if ((event.text.unicode >= '0' && event.text.unicode <= '9')
-            || !(event.text.unicode == '0' && textualValue == "0")
             ||  (event.text.unicode == '.' && !seenDot)) {
                 if (textualValue == "0" && event.text.unicode != '.') {
                     textualValue.pop_back();
                 }
+
                 textualValue += event.text.unicode;
             }
             if (event.text.unicode == '.') {
@@ -44,17 +48,29 @@ void NumericInput::processEvent(const sf::Event& event)
         }
 
         text.setString(textualValue);
+    } else if (event.type == sf::Event::MouseButtonPressed) {
+        focused = rectangle.getGlobalBounds().contains(event.mouseButton.x, event.mouseButton.y);
+        if (focused) {
+            rectangle.setOutlineColor(sf::Color(0x1C62B9FF));
+        } else {
+            rectangle.setOutlineColor(sf::Color(0xCCCCCCFF));
+        }
     }
+
+    bounds.setSize(sf::Vector2f(text.getGlobalBounds().width, text.getGlobalBounds().height));
+    bounds.setPosition(text.getGlobalBounds().left, text.getGlobalBounds().top);
 }
 
 void NumericInput::draw(sf::RenderTarget& target, sf::RenderStates states) const
 {
     target.draw(rectangle);
     target.draw(text);
+    target.draw(bounds);
 }
 
 void NumericInput::setPosition(const sf::Vector2f& pos)
 {
     rectangle.setPosition(pos);
     text.setPosition(pos.x + 2, pos.y);
+    bounds.setPosition(text.getGlobalBounds().left, text.getGlobalBounds().top);
 }
--- PathEvolution/numericinput.h
+++ PathEvolution/numericinput.h
@@ -6,10 +6,12 @@
 class NumericInput : public sf::Drawable
 {
 private:
+    bool focused = false;
     bool seenDot = false;
     std::string textualValue = "0";
     sf::RectangleShape rectangle;
     sf::Text text;
+    sf::RectangleShape bounds;
 
 public:
     NumericInput(float width);
